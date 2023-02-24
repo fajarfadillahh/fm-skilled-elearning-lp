cfcd65b8914294376ae041d0e1bcc274e6ce980e
spossner <github@possner.de>
2023-02-24 08:00:53 +0100

    build: load the tailwind base.css - build now works locally

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
+  integrations: [tailwind()],
 });
